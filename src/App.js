522884a22db98fb4df7fa8f7dfbc0e7b9de1b03d
Dulce Hernandez <dulce.hernandezc@gmail.com>
2019-10-06 19:07:39 -0500

    Add Screen container

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from 'lib/redux';
 
-import PureTaskList from 'components/TaskList/TaskList';
+import InboxScreen from "components/InboxScreen/InboxScreen";
 
 function App() {
   return (
     <Provider store={store}>
-      <PureTaskList />
+      <InboxScreen />
     </Provider>
   );
 }
